cb641c2c346bec3b13e33110ca16180a60802c53
alepacc <alessiopacconi@gmail.com>
2025-11-19 12:44:51 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,69 +1,127 @@
-const grid = document.querySelector('.grid')
-const tips = document.querySelectorAll('.tip')
+const billInput = document.getElementById("bill")
+const peopleInput = document.getElementById("num_people")
+const tips = document.querySelectorAll(".tip")
+const customTip = document.querySelector(".tip--custom input")
 
-const billInput = document.getElementById('bill')
-const peopleInput = document.getElementById('num_people')
+const tipRes = document.querySelector('.result-tip')
+const totalRes = document.querySelector('.result-total')
+const errorMsg = document.querySelector("#error")
+
+const resetBtn = document.querySelector('button[type="reset"]')
+
+function validatePeople(){
+    const peopleValue = peopleInput.value
+
+    if (!peopleValue || peopleValue === '0'){
+        errorMsg.style.display = "block"
+        peopleInput.style.border = "1px solid orange"
+        tipRes.innerText = "$0.00"
+        totalRes.innerText = "$0.00"
+        return
+    } else{
+        errorMsg.style.display = "none"
+        peopleInput.style.border = ""
+    }
+}
+
+function split(percentage){
+    const billValue = billInput.value
+    const peopleValue = peopleInput.value
+
+    // if (!peopleValue || peopleValue === '0'){
+    //     errorMsg.style.display = "block"
+    //     peopleInput.style.border = "1px solid orange"
+    //     return
+    // }
+    // else{
+    //     errorMsg.style.display = "none"
+    //     peopleInput.style.border = ""
+    // }
+    validatePeople()
+
+
+    if (!bill || !percentage || peopleValue === "0" || !peopleValue) {
+        tipRes.innerText = "$0.00"
+        totalRes.innerText = "$0.00"
+        return
+    }
+
+    let percent = parseInt(percentage)/100 
+
+    let tipAmount = +(billValue*percent/peopleValue).toFixed(2)
+    let tot = +(tipAmount+billValue/peopleValue).toFixed(2)
+
+    tipRes.innerText = `$${tipAmount}`
+    totalRes.innerText = `$${tot}`
+}
+
+function rmSecetedTip(){
+    tips.forEach(btn => btn.classList.remove('selected'))
+}
+
+function getPercentage(){
+    const selected = document.querySelector('.selected')
+
+    if (selected) 
+        return parseInt(selected.innerText.replace("%", ""))
+
+    if (customTip.value != "")
+        return parseInt(customTip.value)
+
+    return null
+}
 
 
 
-
-tips.forEach(tip => {
-    tip.addEventListener('click', e => {
-        e.preventDefault()
-
-        tips.forEach(t => t.classList.remove('selected'))
-
+// ––––– EVENTS –––––––
+tips.forEach(tip =>
+    tip.addEventListener("click", () =>  {
+        rmSecetedTip()
         tip.classList.add('selected')
+        customTip.value = ''
 
-        const billValue = billInput.value
-        const peopleValue = peopleInput.value
-        const customTip = document.querySelector('.tip--custom input')
+        // validatePeople()
 
-        if(peopleValue === '0'){
-            document.querySelector('#error').style.display = "block"
-            document.getElementById("num_people").style.border = "1px solid orange"
-        } else if (billValue != '' && peopleValue != ''){
-            if (customTip.value != ''){
-
-            }else {
-                const tipRes = document.querySelector('.result-tip')
-                const totalRes = document.querySelector('.result-total')
-
-                let numb = tip.innerText // read % of the tip selected
-                let percent = numb.replace('%', '')
-                percent = parseInt(percent)/100 
-
-                let tipAmount = +(billValue*percent/peopleValue).toFixed(2)
-                let tot = +(tipAmount+billValue/peopleValue).toFixed(2)
-                tipRes.innerText = `$${tipAmount}`
-                totalRes.innerText = `$${tot}`
-            }
-        } 
-
+        let percent = getPercentage()
+        split(percent)
     })
-})
+)
 
 const inputs = document.querySelectorAll('input')
 inputs.forEach(input => {
-    
+    input.addEventListener('change', e => {
+        e.preventDefault()
+
+        if(customTip.value !== ''){
+            rmSecetedTip()
+        }
+            
+        if(input.value != '' && peopleInput.value !== '0'){
+            let percent = getPercentage()
+            if (percent) split(percent)
+
+        }else {
+            validatePeople()
+        }
+    })
 })
+
   
 // button reset
-const resetBtn = document.querySelector('button[type="reset"]')
 resetBtn.addEventListener('click', e => {
     e.preventDefault()
 
     billInput.value = ''
     peopleInput.value = ''
 
-    document.querySelector('.result-tip').innerText = '$0.00'
-    document.querySelector('.result-total').innerText = '$0.00'
+    tipRes.innerText = '$0.00'
+    totalRes.innerText = '$0.00'
 
     tips.forEach(t => t.classList.remove('selected'))
 
-    document.querySelector('#error').style.display = "none"
-    document.getElementById("num_people").style.border = ''
+    errorMsg.style.display = "none"
+    peopleInput.style.border = ""
 
-    document.querySelector('.tip--custom input').value = ''
+    customTip.value = ''
 
 })
